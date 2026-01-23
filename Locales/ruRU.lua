422aaf030f3bdd556de935cba7238e2bd6c4f119
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-01-24 00:18:44 +0300

    Create ruRU.lua (#1)

---
+++ Locales/ruRU.lua
@@ -0,0 +1,4 @@
+local addonName = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "ruRU", false)
+--Translator ZamestoTV
+L['DEFAULT_GUILD_WELCOME'] = 'Добро пожаловать, PLAYERNAME, в GUILDNAME!'
